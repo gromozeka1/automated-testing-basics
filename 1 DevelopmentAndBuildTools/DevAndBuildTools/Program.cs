37af3616581f8fc719c4e4cddc00dcafb5fa1cd7
nekrashevich.andrei <nekrashevich.andrei@gmail.com>
2023-10-02 21:06:29 +0200

    Add methods to calculate consecutive Latin letters and digits

--- 1 DevelopmentAndBuildTools/DevAndBuildTools/Program.cs
+++ 1 DevelopmentAndBuildTools/DevAndBuildTools/Program.cs
@@ -4,14 +4,25 @@
     {
         Console.Write("Please enter symbols: ");
         string input = Console.ReadLine() ?? throw new ArgumentNullException();
-        
+
         Console.Write("Count of unique symbols: ");
         Console.WriteLine(GetTotalNumberUniqueChars(input));
 
         Console.Write("Count of unique symbols in sequence: ");
         Console.WriteLine(GetMaxNumberUniqueCharsSeq(input));
+
+        Console.Write("Count the maximum number of consecutive identical letters of the Latin alphabet in a line: ");
+        Console.WriteLine(GetMaxNumberConsecutiveIdenticalLatinLetters(input));
+
+        Console.Write("Count the maximum number of consecutive identical digits in a line: ");
+        Console.WriteLine(GetMaxNumberConsecutiveIdenticalDigits(input));
     }
 
+    /// <summary>
+    /// Calculate total number of unique chars in source string.
+    /// </summary>
+    /// <param name="source">Source string</param>
+    /// <returns>Number of unique chars</returns>
     private static int GetTotalNumberUniqueChars(string source)
     {
         HashSet<char> uniqueChars = new();
@@ -23,6 +34,11 @@
         return uniqueChars.Count;
     }
 
+    /// <summary>
+    /// Calculate maximum number of unique consecutive chars in source string.
+    /// </summary>
+    /// <param name="source">Source string</param>
+    /// <returns>Maximum number of unique consecutive chars</returns>
     private static int GetMaxNumberUniqueCharsSeq(string source)
     {
         int max = 1;
@@ -42,5 +58,62 @@
         }
 
         return Math.Max(max, totalMax);
+    }
+
+    /// <summary>
+    /// Calculate the maximum number of consecutive identical letters of the Latin alphabet in a line
+    /// </summary>
+    /// <param name="source">Source string</param>
+    /// <returns>Maximum number of consecutive identical letters of the Latin alphabet</returns>
+    private static int GetMaxNumberConsecutiveIdenticalLatinLetters(string source)
+    {
+        int totalMax = 0;
+        int counter = 0;
+        for (int i = 0; i < source.Length - 1; i++)
+        {
+            if (IsLatinLetter(source[i]) && source[i] == source[i + 1])
+            {
+                counter++;
+                while (source[i] == source[i + 1])
+                {
+                    counter++;
+                    i++;
+                }
+            }
+            else
+            {
+                totalMax = Math.Max(totalMax, counter);
+                counter = 0;
+            }
+        }
+
+        return Math.Max(counter, totalMax);
+
+        static bool IsLatinLetter(char c) => c is (>= 'a' and <= 'z') or (>= 'A' and <= 'Z');
+    }
+
+    /// <summary>
+    /// Calculate the maximum number of consecutive identical digits in a line
+    /// </summary>
+    /// <param name="source">Source string</param>
+    /// <returns>Naximum number of consecutive identical digits</returns>
+    private static int GetMaxNumberConsecutiveIdenticalDigits(string source)
+    {
+        int totalMax = 0;
+        int counter = 0;
+        for (int i = 0; i < source.Length - 1; i++)
+        {
+            if (char.IsDigit(source[i]) && source[i] == source[i + 1])
+            {
+                counter++;
+            }
+            else
+            {
+                totalMax = Math.Max(totalMax, counter);
+                counter = 0;
+            }
+        }
+
+        return Math.Max(counter, totalMax);
     }
 }
